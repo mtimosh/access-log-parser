b3ccbe3b42bd0dbaffdefe6524bed2deb56ff135
Marina Timosh <MTimosh@inno.tech>
2026-01-29 09:44:45 +0300

    Entering numbers and work with numerical operations

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,18 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите первое число:");
+        int firstNumber = scanner.nextInt();
+        System.out.println("Введите второе число:");
+        int secondNumber = scanner.nextInt();
+        int sum = firstNumber + secondNumber;
+        int difference = firstNumber - secondNumber;
+        int product = firstNumber * secondNumber;
+        double quotient = (double) firstNumber / secondNumber;
+        System.out.println("Сумма: " + sum);
+        System.out.println("Разность: " + difference);
+        System.out.println("Произведение: " + product);
+        System.out.println("Частное: " + quotient);
     }
 }
